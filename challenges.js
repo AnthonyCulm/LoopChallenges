ebeab53708c117bb72f64c53d52a1886445ba8b2
AnthonyCulm <Anthonyculm@yahoo.com>
2023-05-13 13:49:39 -0400

    first commit

---
+++ challenges.js
@@ -0,0 +1,27 @@
+//question 1
+for(var i=1;i<20;i+=2){
+    console.log(i);
+}
+//question 2
+for(var x=0;x<100;x++){
+    if(x%3==0){
+        console.log(x);
+    }
+}
+//question 3
+for(var y=4;y>-4;y-=1.5){
+    console.log(y);
+}
+//question 4
+var sum=0
+for(var z=0;z<101;z++){
+    sum+=z
+}
+console.log(sum)
+//question 5
+var product=1
+for(var z=1;z<13;z++){
+    product*=z
+}
+console.log(product)
+
